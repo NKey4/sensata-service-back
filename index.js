6792ee409c33ef3caf7bb8e4203b3ebc8a8d4d3a
Nurbek <117496813+NKey4@users.noreply.github.com>
2024-03-23 21:33:16 +0500

    /

--- index.js
+++ index.js
@@ -10,19 +10,15 @@ const { check, handleValidationErrors } = require("./utils/index");
 
 const { UserController, PostController } = require("./controllers/index");
 
-mongoose
-  .connect(
-    "mongodb+srv://Nurbek:Nurbek123456789@alicebot.qvadg00.mongodb.net/AliceBot?retryWrites=true&w=majority&appName=AliceBot"
-  )
-  .then(() => {
-    console.log("Mongoose success");
-  });
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+  console.log("Mongoose success");
+});
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.listen(3001, console.log("Server has started"));
+app.listen(procces.env.PORT || 3001, console.log("Server has started"));
 
 app.post(
   "/auth/sendCode",
